729b78752178e614f0a9968b5b6ecc112cc8cf19
Karmugilan <164208904+Karmugilan015@users.noreply.github.com>
2026-09-25 19:09:58 +0530

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: 'https://auction-hub-1-jdyt.onrender.com', // Replace with your frontend URL
+  origin: 'http://localhost:3000', // Replace with your frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
