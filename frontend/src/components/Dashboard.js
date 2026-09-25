52cad7d02c28e482d1570ce2fc744fd468a611d5
Karmugilan <164208904+Karmugilan015@users.noreply.github.com>
2026-09-25 18:14:43 +0530

    Update API endpoint for fetching auctions

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -10,7 +10,7 @@ function Dashboard() {
 
     const fetchItems = async () => {
       try {
-        const res = await axios.get('http://localhost:5001/auctions');
+        const res = await axios.get('https://auction-hub-hldk.onrender.com/auctions');
         setItems(res.data);
       } catch (error) {
         console.error('Error fetching auctions:', error);
